b1d5fb7df8dec8781ea205a83913031dbab72702
kevthehermit <kevthehermit@gmail.com>
2014-08-25 14:00:03 -0700

    Update to handle versions 1.0.3

--- NanoCore.py
+++ NanoCore.py
@@ -4,9 +4,9 @@ NanoCore Rat Config Decoder
 '''
 
 
-__description__ = 'xRat Rat Rat Config Extractor'
+__description__ = 'NanoCoreRat Config Extractor'
 __author__ = 'Kevin Breen http://techanarchy.net http://malwareconfig.com'
-__version__ = '0.1'
+__version__ = '0.2'
 __date__ = '2014/09/25'
 
 #Standard Imports Go Here
@@ -33,21 +33,41 @@ Must return a python dict of values
 def run(raw_data):
     try:
         coded_config = get_codedconfig(raw_data)
-        key = coded_config[4:12]
-        data = coded_config[16:]
-        raw_config = decrypt_des(key, data)
-        # if the config is over a certain size it is compressed. Indicated by a non Null byte
-        if raw_config[1] == '\x00':
-            return parse_config(raw_config)
+        if coded_config[0:4] == '\x08\x00\x00\x00':
+            config_dict = decrypt_new(coded_config)
         else:
-            # remove the string lengths and deflate the remainder of the stream
-            deflate_string = raw_config[5:]
-            deflate_config = zlib.decompress(deflate_string, -15)
-            return parse_config(deflate_config)
+           config_dict = decrypt_old(coded_config)
+        return config_dict
     except:
         return
 
 #Helper Functions Go Here
+
+def decrypt_new(coded_config):
+    key = coded_config[4:12]
+    data = coded_config[16:]
+    raw_config = decrypt_des(key, data)
+    # if the config is over a certain size it is compressed. Indicated by a non Null byte
+    if raw_config[1] == '\x00':
+        return parse_config(raw_config, 'new')
+    else:
+        # remove the string lengths and deflate the remainder of the stream
+        deflate_config = deflate_contents(raw_config)
+        return parse_config(deflate_config, 'new')
+            
+            
+def decrypt_old(coded_config):
+    key = '\x01\x03\x05\x08\x0d\x15\x22\x37'
+    data = coded_config[1:]
+    new_data = decrypt_des(key, data)
+    if new_data[0] != '\x00':
+        deflate_config = deflate_contents(new_data)
+        return parse_config(deflate_config, 'old')
+
+
+def deflate_contents(data):
+    new_data = data[5:]
+    return zlib.decompress(new_data, -15)
 
 # Returns only printable chars
 def string_print(line):
@@ -57,33 +77,40 @@ def string_print(line):
         return line
 
 # returns pretty config
-def parse_config(raw_config):
+def parse_config(raw_config, ver):
     config_dict = {}
-    #config_dict['BuildTime'] = unpack(">Q", re.search('BuildTime(.*?)\x0c', raw_config).group()[10:-1])[0]
-    config_dict['Version'] = re.search('Version\x0c(.*?)\x0c', raw_config).group()[8:-1]
-    config_dict['Mutex'] = re.search('Mutex(.*?)\x0c', raw_config).group()[6:-1].encode('hex')
-    config_dict['Group'] = re.search('DefaultGroup\x0c(.*?)\x0c', raw_config).group()[14:-1]
-    config_dict['Domain1'] = re.search('PrimaryConnectionHost\x0c(.*?)\x0c', raw_config).group()[23:-1]
-    config_dict['Domain2'] = re.search('BackupConnectionHost\x0c(.*?)\x0c', raw_config).group()[22:-1]
-    config_dict['Port'] = unpack("<H", re.search('ConnectionPort(.*?)\x0c', raw_config).group()[15:-1])[0]
-    config_dict['RunOnStartup'] = re.search('RunOnStartup(.*?)\x0c', raw_config).group()[13:-1].encode('hex')
-    config_dict['RequestElevation'] = re.search('RequestElevation(.*?)\x0c', raw_config).group()[17:-1].encode('hex')
-    config_dict['BypassUAC'] = re.search('BypassUserAccountControl(.*?)\x0c', raw_config).group()[25:-1].encode('hex')
-    config_dict['ClearZoneIdentifier'] = re.search('ClearZoneIdentifier(.*?)\x0c', raw_config).group()[20:-1].encode('hex')
-    config_dict['ClearAccessControl'] = re.search('ClearAccessControl(.*?)\x0c', raw_config).group()[19:-1].encode('hex')
-    config_dict['SetCriticalProcess'] = re.search('SetCriticalProcess(.*?)\x0c', raw_config).group()[19:-1].encode('hex')
-    config_dict['FindLanServers'] = re.search('FindLanServers(.*?)\x0c', raw_config).group()[15:-1].encode('hex')
-    config_dict['RestartOnException'] = re.search('RestartOnException(.*?)\x0c', raw_config).group()[19:-1].encode('hex')
-    config_dict['EnableDebugMode'] = re.search('EnableDebugMode(.*?)\x0c', raw_config).group()[16:-1].encode('hex')
-    config_dict['ConnectDelay'] = unpack("<i", re.search('ConnectDelay(.*?)\x0c', raw_config).group()[13:-1])[0]
-    config_dict['RestartDelay'] = unpack("<i", re.search('RestartDelay(.*?)\x0c', raw_config).group()[13:-1])[0]
-    #config_dict['TimeoutInterval'] = unpack("<i", re.search('TimeoutInterval(.*?)\x0c', raw_config).group()[16:-1])[0]
-    #config_dict['KeepAliveTimeout'] = unpack("<i", re.search('KeepAliveTimeout(.*?)\x0c', raw_config).group()[17:-1])[0]
-    #config_dict['MutexTimeout'] = unpack("<i", re.search('MutexTimeout(.*?)\x0c', raw_config).group()[13:-1])[0]
-    #config_dict['LanTimeout'] = unpack("<i", re.search('LanTimeout(.*?)\x0c', raw_config).group()[11:-1])[0]
-    #config_dict['WanTimeout'] = unpack("<i", re.search('WanTimeout(.*?)\x0c', raw_config).group()[11:-1])[0]
-    #config_dict['BufferSize'] = unpack("<i", re.search('BufferSize(.*?)\x0c', raw_config).group()[11:-1])[0]
-    #config_dict['MaxPacketSize'] = unpack("<i", re.search('MaxPacketSize(.*?)\x0c', raw_config).group()[14:-1])[0]
+    if ver == 'new':
+        #config_dict['BuildTime'] = unpack(">Q", re.search('BuildTime(.*?)\x0c', raw_config).group()[10:-1])[0]
+        config_dict['Version'] = re.search('Version\x0c(.*?)\x0c', raw_config).group()[8:-1]
+        config_dict['Mutex'] = re.search('Mutex(.*?)\x0c', raw_config).group()[6:-1].encode('hex')
+        config_dict['Group'] = re.search('DefaultGroup\x0c(.*?)\x0c', raw_config).group()[14:-1]
+        config_dict['Domain1'] = re.search('PrimaryConnectionHost\x0c(.*?)\x0c', raw_config).group()[23:-1]
+        config_dict['Domain2'] = re.search('BackupConnectionHost\x0c(.*?)\x0c', raw_config).group()[22:-1]
+        config_dict['Port'] = unpack("<H", re.search('ConnectionPort(.*?)\x0c', raw_config).group()[15:-1])[0]
+        config_dict['RunOnStartup'] = re.search('RunOnStartup(.*?)\x0c', raw_config).group()[13:-1].encode('hex')
+        config_dict['RequestElevation'] = re.search('RequestElevation(.*?)\x0c', raw_config).group()[17:-1].encode('hex')
+        config_dict['BypassUAC'] = re.search('BypassUserAccountControl(.*?)\x0c', raw_config).group()[25:-1].encode('hex')
+        config_dict['ClearZoneIdentifier'] = re.search('ClearZoneIdentifier(.*?)\x0c', raw_config).group()[20:-1].encode('hex')
+        config_dict['ClearAccessControl'] = re.search('ClearAccessControl(.*?)\x0c', raw_config).group()[19:-1].encode('hex')
+        config_dict['SetCriticalProcess'] = re.search('SetCriticalProcess(.*?)\x0c', raw_config).group()[19:-1].encode('hex')
+        config_dict['FindLanServers'] = re.search('FindLanServers(.*?)\x0c', raw_config).group()[15:-1].encode('hex')
+        config_dict['RestartOnException'] = re.search('RestartOnException(.*?)\x0c', raw_config).group()[19:-1].encode('hex')
+        config_dict['EnableDebugMode'] = re.search('EnableDebugMode(.*?)\x0c', raw_config).group()[16:-1].encode('hex')
+        config_dict['ConnectDelay'] = unpack("<i", re.search('ConnectDelay(.*?)\x0c', raw_config).group()[13:-1])[0]
+        config_dict['RestartDelay'] = unpack("<i", re.search('RestartDelay(.*?)\x0c', raw_config).group()[13:-1])[0]
+        #config_dict['TimeoutInterval'] = unpack("<i", re.search('TimeoutInterval(.*?)\x0c', raw_config).group()[16:-1])[0]
+        #config_dict['KeepAliveTimeout'] = unpack("<i", re.search('KeepAliveTimeout(.*?)\x0c', raw_config).group()[17:-1])[0]
+        #config_dict['MutexTimeout'] = unpack("<i", re.search('MutexTimeout(.*?)\x0c', raw_config).group()[13:-1])[0]
+        #config_dict['LanTimeout'] = unpack("<i", re.search('LanTimeout(.*?)\x0c', raw_config).group()[11:-1])[0]
+        #config_dict['WanTimeout'] = unpack("<i", re.search('WanTimeout(.*?)\x0c', raw_config).group()[11:-1])[0]
+        #config_dict['BufferSize'] = unpack("<i", re.search('BufferSize(.*?)\x0c', raw_config).group()[11:-1])[0]
+        #config_dict['MaxPacketSize'] = unpack("<i", re.search('MaxPacketSize(.*?)\x0c', raw_config).group()[14:-1])[0]
+    else:
+        config_dict['Domain'] = re.search('HOST\x0c(.*?)\x0c', raw_config).group()[6:-1]
+        config_dict['Port'] = unpack("<H", re.search('PORT(.*?)\x0c', raw_config).group()[5:-1])[0]
+        config_dict['Group'] = re.search('GROUP\x0c(.*?)\x0c', raw_config).group()[7:-1]
+        config_dict['ConnectDelay'] = unpack("<i", re.search('DELAY(.*?)\x0c', raw_config).group()[6:-1])[0]
+        config_dict['OfflineKeyLog'] = str(re.search('OFFLINE_KEYLOGGING(.*?)\x0c', raw_config).group()[19:-1].encode('hex'))
     return config_dict
 
 
