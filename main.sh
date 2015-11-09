5b534c8fe0f7ecd0a667d351254579ac76a2755a
Michael Hardy <98.michael.hardy@gmail.com>
2015-11-08 16:13:37 -0800

    Added a bash script to test one file for all RATs added .gitignore for virtualenv

---
+++ main.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+source venv/bin/activate
+
+python adWind.py $@
+python Adzok.py $@
+python Albertino.py $@
+python AlienSpy.py $@
+python Arcom.py $@
+python BlackNix.py $@
+python BlackShades.py $@
+python BlueBanana.py $@
+python Bozok.py $@
+python ClientMesh.py $@
+python Crimson.py $@
+python CyberGate.py $@
+python DarkComet.py $@
+python darkddoser.py $@
+python DarkRAT.py $@
+python Gh0st.py $@
+python Greame.py $@
+python HawkEye.py $@
+python JavaDropper.py $@
+python jRAT.py $@
+python LostDoor.py $@
+python LuxNet.py $@
+python NanoCore.py $@
+python njRat.py $@
+python Pandora.py $@
+python PoisonIvy.py $@
+python PredatorPain.py $@
+python Punisher.py $@
+python QRat.py $@
+python ShadowTech.py $@
+python SmallNet.py $@
+python SpyGate.py $@
+python Tapaoux.py $@
+python unrecom.py $@
+python Vantom.py $@
+python VirusRat.py $@
+python Xena.py $@
+python xRat.py $@
+python Xtreme.py $@
